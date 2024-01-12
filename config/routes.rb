fd61426455ba6c62c9edb2ee9c898ce2b2a56211
obarikuto <sausupo0404@gmail.com>
2024-01-12 19:59:54 +0900

    フッター作成 お問い合わせフォーム途中

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,19 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   get 'logout', to: 'sessions#destroy', as: :logout_get
 
+  # フッターのリンクを作成
+  get 'privacy', to: 'pages#privacy', as: :privacy
+  get 'terms', to: 'pages#terms', as: :terms
+
+  # お問い合わせメール
+  namespace :public do
+    resources :contacts, only: [:new, :create] do
+      collection do
+        post 'confirm'
+        post 'back'
+        get 'done'
+      end
+    end
+  end
+
 end
