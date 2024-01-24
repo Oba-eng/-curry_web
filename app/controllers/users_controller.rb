4f192b75f939df99a4b8b3159a2b7cc9630ad49d
obarikuto <sausupo0404@gmail.com>
2024-01-24 13:40:27 +0900

    ユーザー作成と同時にログイン

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,16 +9,18 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id 
       redirect_to menus_path, success: 'サインアップしました'
     else
       flash.now[:danger] = 'サインアップに失敗しました'
       render :new
     end
   end
-
+    
   private
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation, :name)
   end
 end
+
