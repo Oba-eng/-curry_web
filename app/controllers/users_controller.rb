184d9470859c3c093dda37ad3e4308ed147ce57e
obarikuto <sausupo0404@gmail.com>
2024-01-26 10:23:25 +0900

    検索機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,14 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  def show
+    @user = User.find(params[:id])
+    @menus = @user.menus
+
+    favorites = Favorite.where(user_id: current_user.id).pluck(:menu_id)  # ログイン中のユーザーのお気に入りのpost_idカラムを取得
+    @favorite_list = Menu.find(favorites)     # menusテーブルから、お気に入り登録済みのレコードを取得
+  end
     
   private
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, uniqueness: true, presence: true
+
+  has_many :menus
+
+  # お気に入り機能
+  has_many :favorites, dependent: :destroy
+
+  def mine?(object)
+    id == object.user_id
+  end
 end
