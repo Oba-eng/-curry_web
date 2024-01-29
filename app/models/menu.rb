ba10ec8e02f20d2a56b45d2a24bb1db192784d08
obarikuto <sausupo0404@gmail.com>
2024-01-29 15:14:05 +0900

    細かい見た目修正

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,6 +1,6 @@
 class Menu < ApplicationRecord
   mount_uploader :menu_image, MenuImageUploader
-
+  
   belongs_to :user
 
   # お気に入り機能
@@ -10,7 +10,7 @@ class Menu < ApplicationRecord
   def favorited_by(user)
     Favorite.find_by(user_id: user.id, menu_id: id)
   end
-  
+
   # 検索機能
   def self.search(query)
     ransack(name_or_material_cont: query).result
