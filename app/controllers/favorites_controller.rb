f9e98576a24797809fa55c96b5fb6a33c2221ea1
obarikuto <sausupo0404@gmail.com>
2024-03-14 18:46:02 +0900

    お気に入りcreateのエラー解消

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,13 +2,13 @@ class FavoritesController < ApplicationController
   before_action :require_login
 
   def create
-    menu = Menu.find(params[:menu_id])
+    menu = Menu.find(params[:id])
     current_user.favorite(menu)
     redirect_back fallback_location: root_path, success: t('defaults.message.bookmark')
   end
 
   def destroy
-    menu = current_user.favorites.find(params[:id]).menu
+    menu = current_user.favorites.find(params[:id])
     current_user.unfavorite(menu)
     redirect_back fallback_location: root_path, success: t('defaults.message.unbookmark')
   end
