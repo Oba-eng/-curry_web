910e42c4d3207e3a7e7a2cc527944cfc5c6cb652
obarikuto <sausupo0404@gmail.com>
2024-04-05 20:31:51 +0900

    Webpackの設定を変更

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,13 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
+
 environment.plugins.prepend('Provide',
     new webpack.ProvidePlugin({
         $: 'jquery/src/jquery',
         jQuery: 'jquery/src/jquery'
     })
 )
+
+environment.entry.set('application', './app/javascript/packs/application.js')
 
 module.exports = environment
