413a0d3a65113811161f1c2921153413bc49b7ab
obarikuto <sausupo0404@gmail.com>
2024-04-05 21:10:42 +0900

    エントリーポイントの設定

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,16 @@
-const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
+const { environment } = require('@rails/webpacker');
+const webpack = require('webpack');
+
+// エントリーポイントの設定
+environment.entry.prepend('application', './app/assets/javascripts/packs/application.js');
+environment.entry.prepend('application', './app/assets/javascripts/application.js');
+
+// jQueryのプラグインを追加
 environment.plugins.prepend('Provide',
     new webpack.ProvidePlugin({
         $: 'jquery/src/jquery',
         jQuery: 'jquery/src/jquery'
     })
-)
+);
 
-module.exports = environment
+module.exports = environment;
