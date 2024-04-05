f218ef9e439fa79fccb598a3fde1d7debf42a21b
obarikuto <sausupo0404@gmail.com>
2024-04-05 21:18:40 +0900

    小さいエラー解消

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { environment } = require('@rails/webpacker');
 const webpack = require('webpack');
 
@@ -7,12 +6,6 @@ environment.entry.prepend('application', './app/assets/javascripts/packs/applica
 environment.entry.prepend('application', './app/assets/javascripts/application.js');
 
 // jQueryのプラグインを追加
-=======
-const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
-
-// ProvidePluginを追加
->>>>>>> bd3727c4ebfd5de6a15a5430f43aca8b26bbe085
 environment.plugins.prepend('Provide',
     new webpack.ProvidePlugin({
         $: 'jquery/src/jquery',
@@ -20,10 +13,4 @@ environment.plugins.prepend('Provide',
     })
 );
 
-<<<<<<< HEAD
 module.exports = environment;
-=======
-environment.entry.set('application', './app/assets/javascripts/application.js')
-
-module.exports = environment
->>>>>>> bd3727c4ebfd5de6a15a5430f43aca8b26bbe085
