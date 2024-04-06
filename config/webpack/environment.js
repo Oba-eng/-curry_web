99d0796a5b749152a9711c987e1516c3896f6390
obarikuto <sausupo0404@gmail.com>
2024-04-06 20:09:12 +0900

    メソッドを変更

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,8 +2,9 @@ const { environment } = require('@rails/webpacker');
 const webpack = require('webpack');
 
 // エントリーポイントの設定
-environment.entry.prepend('application', './app/assets/javascripts/packs/application.js');
-environment.entry.prepend('application', './app/assets/javascripts/application.js');
+environment.entryPoints
+  .prepend('application', './app/assets/javascripts/packs/application.js')
+  .prepend('application', './app/assets/javascripts/application.js');
 
 // jQueryのプラグインを追加
 environment.plugins.prepend('Provide',
