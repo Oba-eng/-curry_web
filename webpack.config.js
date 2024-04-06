a482ae89d33ccfecd6481cb88a0d7258a5a5ed6e
obarikuto <sausupo0404@gmail.com>
2024-04-06 21:01:50 +0900

    webpack.config.jsの設定を変更した

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,15 @@
+const path = require('path');
+
 module.exports = {
-  entry: './app/javascript/packs/application.js', // あるいは他の適切なエントリーポイントへのパス
-  // ... 他の設定オプション ...
+  entry: './app/assets/javascripts/packs/application.js', // エントリーポイントのパスを設定
+  output: {
+    filename: 'bundle.js', // バンドルされたJavaScriptファイルの名前
+    path: path.resolve(__dirname, 'public/js'), // バンドルされたJavaScriptファイルの出力先ディレクトリ
+  },
+  module: {
+    rules: [
+      // ローダーの設定などが必要であればここに記述
+    ],
+  },
+  // プラグインやその他の設定が必要であればここに記述
 };
