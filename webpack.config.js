1fdf7ebc34ccea8830dbd27bcd8195e91fa79ded
obarikuto <sausupo0404@gmail.com>
2024-04-09 01:07:33 +0900

    webpack.config.jsの内容を変更

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   entry: './app/javascript/packs/application.js', // あるいは他の適切なエントリーポイントへのパス
   // ... 他の設定オプション ...
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      // 他のローダーの設定...
+    ],
+  },
 };
