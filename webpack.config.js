ff236c86f2548dbffafa85108e82e96198726871
obarikuto <sausupo0404@gmail.com>
2024-04-09 09:56:11 +0900

    webpack.config.jsの設定を変更した

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,4 @@ module.exports = {
     application: './app/javascript/packs/application.js',
     menus: './app/javascript/packs/menus.js',
   },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      // 他のローダーの設定...
-    ],
-  },
 };
