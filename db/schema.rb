3eec791d7adc5a4e00f23699acfa9f370629514c
obarikuto <sausupo0404@gmail.com>
2024-04-09 18:16:02 +0900

    いらないファイルを消去したりした

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_08_103038) do
+ActiveRecord::Schema.define(version: 2024_04_09_090850) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "name", null: false
@@ -50,8 +50,6 @@ ActiveRecord::Schema.define(version: 2024_03_08_103038) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "crypted_password", default: "", null: false
-    t.string "salt", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
