44046596d1c4c006b47a48350a6493d065ad764f
obarikuto <sausupo0404@gmail.com>
2024-04-10 17:12:40 +0900

    db修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_08_103038) do
+ActiveRecord::Schema.define(version: 2024_04_10_081149) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "name", null: false
@@ -33,10 +33,10 @@ ActiveRecord::Schema.define(version: 2024_03_08_103038) do
     t.string "material", null: false
     t.text "make", null: false
     t.text "point", null: false
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "menu_image"
+    t.integer "user_id"
     t.index ["user_id"], name: "index_menus_on_user_id"
   end
 
@@ -50,13 +50,11 @@ ActiveRecord::Schema.define(version: 2024_03_08_103038) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "crypted_password", default: "", null: false
-    t.string "salt", default: "", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "favorites", "menus"
   add_foreign_key "favorites", "users"
-  add_foreign_key "menus", "users"
 end
