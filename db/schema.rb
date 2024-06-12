8559d74509666223a9f1b088c46350615233e2f3
obarikuto <sausupo0404@gmail.com>
2024-06-12 13:16:57 +0900

    remove:不要なファイル削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_06_085307) do
+ActiveRecord::Schema.define(version: 2024_05_20_014130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "authentications", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "provider", null: false
-    t.string "uid", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["provider", "uid"], name: "index_authentications_on_provider_and_uid"
-  end
 
   create_table "contacts", force: :cascade do |t|
     t.string "name", null: false
